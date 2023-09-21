d806ad3b5c2062eb8b06189be004344fdde1f88d
Toriqul-FDW-1 <idhofirdauz11@gmail.com>
2023-09-21 15:52:25 +0800

    Tugas 1 PHP

--- latihan1.php
+++ latihan1.php
@@ -1,5 +1,29 @@
 <?php
+//contoh komentar di php
+// echo "hello world";
 
-echo "hello world";
+
+//latihan membuat variabel
+$namaSiswa = "Toriqul Firdaus";//tipe data string
+$umur = 22;//tipe data integer
+$beratBadan = 55.5;//tipe data float
+$_pekerjaan= 'Mahasiswa';
+
+$jari2 = 15;
+define('PHI', 3.14);
+
+$luas = PHI * $jari2 * $jari2;
+echo 'Nama Mahasiswa : '.$namaSiswa;
+echo '<br>Umur : '.$umur;
+echo '<br>Berat Badan : '.$beratBadan;
+echo '<br>Pekerjaan : '.$_pekerjaan;
+echo '<br> Berat Badan 2 : '.$beratBadan, ' kg';
+
 
 ?>
+<br> Siswa : 1
+<?= $namaSiswa ?>
+<br> Umur :
+<?= $umur ?>
+<br> Luas jari-jari :
+<?= $luas ?>
