dcfff229bea5544a0f6bfb8db7dd8689d5857813
Toriqul-FDW-1 <idhofirdauz11@gmail.com>
2023-11-09 13:06:16 +0800

    edit update delete

--- laravel/app/Http/Controllers/ProdukController.php
+++ laravel/app/Http/Controllers/ProdukController.php
@@ -24,12 +24,7 @@ class ProdukController extends Controller
         return view('admin.produk.create', compact('jenis_produk'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         //tambah data menggunakan query builder
@@ -44,4 +39,40 @@ class ProdukController extends Controller
         ]);
         return redirect('admin/produk');
     }
+
+    public function show(string $id){
+        $produk = Produk::join('jenis_produk', 'jenis_produk_id', '=', 'jenis_produk.id')
+        ->select('produk.*', 'jenis_produk.nama as jenis')
+        ->where('produk.id', $id)
+        ->get();
+        return view ('admin.produk.detail', compact('produk'));
+    }
+
+    public function edit(string $id){
+        $jenis_produk = DB::table('jenis_produk')->get();
+        $produk = DB::table('produk')->where('id', $id)->get();
+        return view('admin.produk.edit', compact('produk', 'jenis_produk'));
+    }
+
+    public function update(Request $request, string $id){
+    DB::table('produk')->where('id',$request->id)->update([
+        'kode'=>$request->kode,
+        'nama'=>$request->nama,
+        'harga_beli'=>$request->harga_beli,
+        'harga_jual'=>$request->harga_jual,
+        'stok'=>$request->stok,
+        'min_stok'=>$request->min_stok,
+        'jenis_produk_id'=>$request->jenis_produk_id,
+    ]);
+    
+    return redirect('admin/produk');
+    }
+
+    public function destroy(string $id){
+        
+        DB::table('produk')->where('id', $id)->delete();
+        return redirect('admin/produk');
+    }
 }
+
+
